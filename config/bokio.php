4a2b1b6a44551fd8d5fb91ae08b83bfe4ce8b28d
Mathias Palmqvist <mathias@mattitja.se>
2025-06-30 10:30:35 +0200

    Updated config file

--- config/bokio.php
+++ config/bokio.php
@@ -1,7 +1,36 @@
 <?php
 
-// config for Mattitja/Bokio
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Bokio API Access Token
+    |--------------------------------------------------------------------------
+    |
+    | Your integration token from Bokio. Found in your Bokio account.
+    |
+    */
+
     'token' => env('BOKIO_TOKEN'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Company ID
+    |--------------------------------------------------------------------------
+    |
+    | You can find this in the Bokio web URL when logged into your company.
+    |
+    */
+
     'company_id' => env('BOKIO_COMPANY_ID'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Bokio API Base URL
+    |--------------------------------------------------------------------------
+    |
+    | You normally don't need to change this.
+    |
+    */
+
+    'base_url' => env('BOKIO_BASE_URL', 'https://api.bokio.se'),
 ];
